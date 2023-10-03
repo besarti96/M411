c5e625c83e3a78b464bde2e3304227b03bcfbb87
Isa Halimi <isa.halimi90@gmail.com>
2023-10-03 21:37:11 +0200

    mongobd connection and add some classes

---
+++ src/main/java/com/example/m411/GUIPersonenBearbeiten.java
@@ -0,0 +1,4 @@
+package com.example.m411;
+
+public class GUIPersonenBearbeiten {
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module com.example.m411 {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires org.mongodb.driver.core;
+    requires org.mongodb.driver.sync.client;
+    requires org.mongodb.bson;
+
+
     opens com.example.m411 to javafx.fxml;
     exports com.example.m411;
 }
