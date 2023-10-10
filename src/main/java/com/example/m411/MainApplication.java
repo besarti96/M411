bf470593d954e3fc2b48fa1a0e3333bf2c642072
Isa Halimi <isa.halimi90@gmail.com>
2023-10-10 19:09:22 +0200

    view main anpassen

--- src/main/java/com/example/m411/MainApplication.java
+++ src/main/java/com/example/m411/MainApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("person_view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("persons_view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello isssa!");
         stage.setScene(scene);
